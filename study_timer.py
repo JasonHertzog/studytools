eb3081a55c1ed4ee13a92ba63951e1d83047f248
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2021-10-03 10:08:43 -0400

    Update study_timer.py
    + Updated timer to actually finish at 15 minutes instead of 16 minutes.
    + Added a few more quotes to help passively learn concepts.
    + Changed the "finished" text to make it more obvious that we are ready to move on.
    + Added Windows beep sounds as an alternative way of informing the user that it's time to move on.

--- study_timer.py
+++ study_timer.py
@@ -1,15 +1,17 @@
-import time, random
+import time, random, winsound
 s = 60
 p = 0
-m = 15
+m = 14
 t = 0
-ran_quote = ["It all starts with an assumption. (Inductive Step)",
+ran_quote = ["It all starts with an assumption.(After Basis) (Inductive Step)",
  "It all ends with a conclusion. (Inductive Step)",
  "The rule of inference behind mathematical induction can be expressed in predicate logic as:\n (P(1)^∀k(P(k)->P(k+1))) -> ∀nP(n)",
  "Step 1: For All... Fixed integer! (Mathematical Induction)"
 ,"Mathematical induction is used to prove a statement true for an (countably) infinite number of cases."
 ,"Remember to factor out dy/dx after taking the derivative."
- ]
+,"To find two points where a curve crosses the x-axis, solve for x first. "
+ ,"65xy = 65x AND 65y."
+ ,"During the basis step, we want to specify variables like P(n)"]
 
 
 print("Start learning! Don't give up for at least one minute.")
@@ -29,8 +31,15 @@ while s >= 0:
 		print(f""+str(t)+" minute(s) have passed.")
 	else:
 		s -= 1
-		print("Time's up! Look up the answer.")
-		print("Time's up! Look up the answer.")
-		print("Time's up! Look up the answer.")
+		print("*"*32)
+		print("*Time's up! Look up the answer.*")
+		print("*"*32)
+		print("*Time's up! Look up the answer.*")
+		print("*"*32)
+		print("*Time's up! Look up the answer.*")
+		print("*"*32)
+		# The following code will sound an alarm. If it's annoying, change it.
+		winsound.Beep(440,750)
+		time.sleep(1)
+		winsound.Beep(440,1000)
 	time.sleep(1)
-
