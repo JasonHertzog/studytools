c9761eab060343959e038c1d337b724b6a89a2f1
JasonHertzog <86388948+JasonHertzog@users.noreply.github.com>
2021-11-15 09:40:49 -0500

    Added Saving / Loading and time manipulation
    Added a way to keep track of how much time has been spent on studying for each area.

--- Study Schedule.py
+++ Study Schedule.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, pickle
 
 # Class1, Class2, Class3, Class4.
 # Calc, CS, Span, Gov
@@ -57,10 +57,79 @@ elif today == 6:
 else:
     print("Something is broken in the datetime day checking loop.")
 
-print(f"Today is",today+", and your schedule for today is:\n"
-+str(class1_name)+":",str(class1_time)+" minutes.\n"
-+str(class2_name)+":",str(class2_time)+" minutes.\n"
-+str(class3_name)+":",str(class3_time)+" minutes.\n"
-+str(class4_name)+":",str(class4_time)+" minutes.\n"
-)
+def todays_schedule():
+    print(f"Today is",today+", and your schedule for today is:\n"
+    +str(class1_name)+":",str(class1_time)+" minutes.\n"
+    +str(class2_name)+":",str(class2_time)+" minutes.\n"
+    +str(class3_name)+":",str(class3_time)+" minutes.\n"
+    +str(class4_name)+":",str(class4_time)+" minutes.\n"
+    )
+def first_check(a, c1, c2, c3, c4):
+    if a == "y" or a =="yes" or a =="ye":
+        print("Let's get our day started!\n")
+        filehandler = open("Study Schedule.sav","wb")
+        pickle.dump(c1, filehandler)
+        pickle.dump(c2, filehandler)
+        pickle.dump(c3, filehandler)
+        pickle.dump(c4, filehandler)
+        filehandler.close()
+        return(0, c1, c2, c3, c4)
+    else:
+        print("Welcome back.")
+        filehandler = open("Study Schedule.sav","rb")
+        c1 = pickle.load(filehandler)
+        c2 = pickle.load(filehandler)
+        c3 = pickle.load(filehandler)
+        c4 = pickle.load(filehandler)
+        filehandler.close()
+        return(0, c1, c2, c3, c4)
+        
+def quicksave():
+            filehandler = open("Study Schedule.sav","wb")
+            c1 = class1_time
+            pickle.dump(c1, filehandler)
+            c2 = class2_time
+            pickle.dump(c2, filehandler)
+            c3 = class3_time
+            pickle.dump(c3, filehandler)
+            c4 = class4_time
+            pickle.dump(c4, filehandler)
+            filehandler.close()
 
+# Main algorithm
+a = input("Is this the first time you're checking your schedule today? y = yes: ").strip().lower()
+a, class1_time, class2_time, class3_time, class4_time = first_check(a, class1_time, class2_time, class3_time, class4_time)
+todays_schedule()
+
+i = 1
+while i > 0:
+    print("Directory: \n0: Check Schedule.\n1: Subtract Time\nt: Terminate script")
+    a = input()
+    if a == str("0"):
+        print('\n')
+        todays_schedule()
+    if a == str("1"):
+        b = input("Subtract time from which class?\n1 = "+class1_name+"\n"+
+        "2 = "+class2_name+"\n"+
+        "3 = "+class3_name+"\n"+
+        "4 = "+class4_name+"\n"
+        )
+        if b == "1":
+            class1_time -= int(input("Time Spent (in minutes): "))
+            quicksave()
+        elif b == "2":
+            class2_time -= int(input("Time Spent (in minutes): "))    
+            quicksave()
+        elif b == "3":
+            class3_time -= int(input("Time Spent (in minutes): "))
+            quicksave()
+        elif b == "4":
+            class4_time -= int(input("Time Spent (in minutes): "))
+            quicksave()
+        else:
+            print("\n")
+
+    elif a == "t":
+        i = 0
+
+input("Finished Running. Press enter to termiate program.")
